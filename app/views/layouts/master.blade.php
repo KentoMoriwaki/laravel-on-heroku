d81b1efc898bb375e5332c4d8b9422c4612ebc05
Kento <kirkland1739@gmail.com>
2014-01-10 09:56:25 +0900

    Add bootstrap and font-awesome

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css"/>
+</head>
+<body>
+
+@yield('content')
+
+<script type="text/javascript" src="/bower_components/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
